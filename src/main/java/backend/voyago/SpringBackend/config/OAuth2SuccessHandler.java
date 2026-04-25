994de12674c925f28a1c068c8cdc111da65b1d8f
Shashwat17-vit <shash14negi@gmail.com>
2026-04-25 16:03:02 -0500

    Added back the code

--- src/main/java/backend/voyago/SpringBackend/config/OAuth2SuccessHandler.java
+++ src/main/java/backend/voyago/SpringBackend/config/OAuth2SuccessHandler.java
@@ -12,21 +12,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class OAuth2SuccessHandler implements AuthenticationSuccessHandler{
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
 
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 
+        String name  = oAuth2User.getAttribute("name") != null
+                       ? oAuth2User.getAttribute("name")
+                       : oAuth2User.getAttribute("login"); // GitHub fallback
         String email = oAuth2User.getAttribute("email");
-        String name = oAuth2User.getAttribute("name");
+
         // TODO Step 6: save user to DB here
+        // TODO Step 7: generate JWT and append as ?token=jwt here
 
-        // TODO Step 7: generate JWT token here
-
-        // redirect back to frontend with token
-        response.sendRedirect("http://localhost:5173/p101/#/login?token=dummy");
-
-    }   
-
+        response.sendRedirect("http://localhost:5173/#/home");
+    }
 }
